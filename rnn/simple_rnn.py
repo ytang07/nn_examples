761d81cff47dfd04c2ea4cb71ecd65aa30e8fb1c
Yujian Tang <ytang07@gmail.com>
2021-12-19 16:21:56 -0800

    correcting locations

---
+++ rnn/simple_rnn.py
@@ -0,0 +1,138 @@
+import math
+import numpy as np
+
+# create RNN architecture
+learning_rate = 0.0001
+seq_len = 50
+nepoch = 25
+hidden_dim = 100
+output_dim = 1
+bptt_truncate = 5 # backprop through time --> lasts 5 iterations
+min_clip_val = -10
+max_clip_val = 10
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+def calculate_loss(X, Y, U, V, W):
+    loss = 0.0
+    for i in range(Y.shape[0]):
+        x, y = X[i], Y[i]
+        prev_activation = np.zeros((hidden_dim, 1)) # value of previous activation
+        for timestep in range(seq_len):
+            new_input = np.zeros(x.shape) # forward pass, done for each step in the sequence
+            new_input[timestep] = x[timestep] # define a single input for that timestep
+            mulu = np.dot(U, new_input)
+            mulw = np.dot(W, prev_activation)
+            add = mulu + mulw
+            activation = sigmoid(add)
+            mulv = np.dot(V, activation)
+            prev_activation = activation
+        # calculate and add loss per record
+        loss_per_record = float((y - mulv)**2/2)
+        loss += loss_per_record
+    # calculate loss after first Y pass
+    loss = loss/float(y.shape[0])
+    return loss, prev_activation
+
+# takes x values and the weights matrices
+# returns layer dictionary, final weights (mulu, mulw, mulv)
+def calc_layers(x, U, V, W, prev_activation):
+    layers = []
+    for timestep in range(seq_len):
+        new_input = np.zeros(x.shape)
+        new_input[timestep] = x[timestep]
+        mulu = np.dot(U, new_input)
+        mulw = np.dot(W, prev_activation)
+        add = mulw + mulu
+        activation = sigmoid(add)
+        mulv = np.dot(V, activation)
+        layers.append({'activation': activation, 'prev_activation': prev_activation})
+        prev_activation = activation
+    
+    return layers, mulu, mulw, mulv
+
+def backprop(x, U, V, W, dmulv, mulu, mulw, layers):
+    dU = np.zeros(U.shape)
+    dV = np.zeros(V.shape)
+    dW = np.zeros(W.shape)
+    
+    dU_t = np.zeros(U.shape)
+    dV_t = np.zeros(V.shape)
+    dW_t = np.zeros(W.shape)
+    
+    dU_i = np.zeros(U.shape)
+    dW_i = np.zeros(W.shape)
+    
+    add = mulu + mulw
+    for timestep in range(seq_len):
+        dV_t = np.dot(dmulv, np.transpose(layers[timestep]['activation']))
+        dsv = np.dot(np.transpose(V), dmulv)
+        ds = dsv
+        dadd = add * (1 - add) * ds
+        dmulw = dadd * np.ones_like(mulw)
+        dprev_activation = np.dot(np.transpose(W), dmulw)
+        
+        for i in range(timestep-1, max(-1, timestep-bptt_truncate-1), -1):
+            ds = dsv + dprev_activation
+            dadd = add * (1-add) * ds
+            dmulw = dadd * np.ones_like(mulw)
+            dW_i = np.dot(W, layers[timestep]['prev_activation'])
+            dprev_activation = np.dot(np.transpose(W), dmulw)
+            
+            new_input = np.zeros(x.shape)
+            new_input[timestep] = x[timestep]
+            dU_i = np.dot(U, new_input)
+            
+            dU_t += dU_i
+            dW_t += dW_i
+            
+        dU += dU_t
+        dV += dV_t
+        dW += dW_t
+        
+        # take care of possible exploding gradients
+        if dU.max() > max_clip_val:
+            dU[dU > max_clip_val] = max_clip_val
+        if dV.max() > max_clip_val:
+            dV[dV > max_clip_val] = max_clip_val
+        if dW.max() > max_clip_val:
+            dW[dW > max_clip_val] = max_clip_val
+        
+        if dU.min() < min_clip_val:
+            dU[dU < min_clip_val] = min_clip_val
+        if dV.min() < min_clip_val:
+            dV[dV < min_clip_val] = min_clip_val
+        if dW.min() < min_clip_val:
+            dW[dW < min_clip_val] = min_clip_val
+        
+    return dU, dV, dW
+
+# training
+def train(U, V, W, X, Y, X_validation, Y_validation):
+    for epoch in range(nepoch):
+        # calculate initial loss, ie what the output is given a random set of weights
+        loss, prev_activation = calculate_loss(X, Y, U, V, W)
+
+        # check validation loss
+        val_loss, prev_activation = calculate_loss(X_validation, Y_validation, U, V, W)
+        
+        print(f'Epoch: {epoch+1}, Loss: {loss}, Validation Loss: {val_loss}')
+
+        # train model/forward pass
+        for i in range(Y.shape[0]):
+            x, y = X[i], Y[i]
+            layers = []
+            prev_activation = np.zeros((hidden_dim, 1))
+            
+            layers, mulu, mulw, mulv = calc_layers(x, U, V, W, prev_activation)
+                
+            # derivative of the prediction
+            dmulv = (mulv - y)
+            dU, dV, dW = backprop(x, U, V, W, dmulv, mulu, mulw, layers)
+            
+            # update weights
+            U -= learning_rate * dU
+            V -= learning_rate * dV
+            W -= learning_rate * dW
+    return U, V, W
---
+++ rnn/test_simple_rnn.py
@@ -0,0 +1,87 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+from sklearn.metrics import mean_squared_error
+
+from simple_rnn import train, hidden_dim, seq_len, sigmoid, output_dim
+
+sin_wave = np.array([math.sin(x) for x in range(200)])
+# training data
+X = []
+Y = []
+num_records = len(sin_wave) - seq_len # 150
+
+# X entries are 50 data points
+# Y entries are the 51st data point
+for i in range(num_records-50):
+    X.append(sin_wave[i:i+seq_len])
+    Y.append(sin_wave[i+seq_len])
+
+X = np.expand_dims(np.array(X), axis=2) # 100 x 50 x 1
+Y = np.expand_dims(np.array(Y), axis=1) # 100 x 1
+
+# validation data
+X_validation = []
+Y_validation = []
+for i in range(num_records-seq_len, num_records):
+    X_validation.append(sin_wave[i:i+seq_len])
+    Y_validation.append(sin_wave[i+seq_len])
+
+X_validation = np.expand_dims(np.array(X_validation), axis=2)
+Y_validation = np.expand_dims(np.array(Y_validation), axis=1)
+
+np.random.seed(12161)
+U = np.random.uniform(0, 1, (hidden_dim, seq_len)) # weights from input to hidden layer
+V = np.random.uniform(0, 1, (output_dim, hidden_dim)) # weights from hidden to output layer
+W = np.random.uniform(0, 1, (hidden_dim, hidden_dim)) # recurrent weights for layer (RNN weigts)
+
+train(U, V, W, X, Y, X_validation, Y_validation)
+
+# predictions on the training set
+predictions = []
+for i in range(Y.shape[0]):
+    x, y = X[i], Y[i]
+    prev_activation = np.zeros((hidden_dim,1))
+    # forward pass
+    for timestep in range(seq_len):
+        mulu = np.dot(U, x)
+        mulw = np.dot(W, prev_activation)
+        add = mulu + mulw
+        activation = sigmoid(add)
+        mulv = np.dot(V, activation)
+        prev_activation = activation
+    predictions.append(mulv)
+
+predictions = np.array(predictions)
+
+plt.plot(predictions[:, 0,0], 'g')
+plt.plot(Y[:, 0], 'r')
+plt.title("Training Data Predictions in Green, Actual in Red")
+plt.show()
+
+# predictions on the validation set
+val_predictions = []
+for i in range(Y_validation.shape[0]):
+    x, y = X[i], Y[i]
+    prev_activation = np.zeros((hidden_dim,1))
+    # forward pass
+    for timestep in range(seq_len):
+        mulu = np.dot(U, x)
+        mulw = np.dot(W, prev_activation)
+        add = mulu + mulw
+        activation = sigmoid(add)
+        mulv = np.dot(V, activation)
+        prev_activation = activation
+    val_predictions.append(mulv)
+
+val_predictions = np.array(val_predictions)
+
+plt.plot(val_predictions[:, 0,0], 'g')
+plt.plot(Y_validation[:, 0], 'r')
+plt.title("Test Data Predictions in Green, Actual Data in Red")
+plt.show()
+
+# check RMSE
+rmse = math.sqrt(mean_squared_error(Y_validation[:,0], val_predictions[:, 0, 0]))
+print(rmse)
