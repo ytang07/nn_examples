cb50270216f7584480b1547c44f1c01dcdbb96a7
Yujian Tang <ytang07@gmail.com>
2021-12-25 14:46:19 -0800

    simple keras rnn

---
+++ rnn/keras_rnn.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+model = keras.Sequential()
+model.add(layers.SimpleRNN(64, input_shape=(None, 28)))
+model.add(layers.BatchNormalization())
+model.add(layers.Dense(10))
+print(model.summary())
+
+mnist = keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train/255.0, x_test/255.0
+sample, sample_label = x_test[0], y_test[0]
+
+model.compile(
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer="sgd",
+    metrics=["accuracy"],
+)
+
+model.fit(
+    x_train, y_train, validation_data=(x_test, y_test), batch_size=64, epochs=1
+)
+
+result = tf.argmax(model.predict(tf.expand_dims(sample, 0)), axis=1)
+print(result.numpy(), sample_label)
